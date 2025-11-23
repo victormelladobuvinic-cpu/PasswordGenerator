7143856ce7daf3dc0faeb16f8107f4f388d434e0
victormelladobuvinic-cpu <victor.melladobuvinic@gmail.com>
2025-11-22 21:28:45 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
+
+
+/*let randomIndex = Math.floor(Math.random() * characters.length)*/
+let numOfCharacters = 15
+let passwordItemsOne = []
+let passwordItemsTwo = []
+let pswOne = document.querySelector('#psw-one')
+let pswTwo = document.querySelector('#psw-two')
+
+function generatePassword() {
+
+for (let i = 0; i < numOfCharacters; i++) {
+    let randomIndex = Math.floor(Math.random() * characters.length)
+    passwordItemsOne.push(characters[randomIndex])
+}
+
+for (let i = 0; i < numOfCharacters; i++) {
+    let randomIndex = Math.floor(Math.random() * characters.length)
+    passwordItemsTwo.push(characters[randomIndex])
+}
+ let passwordOne = passwordItemsOne.join('')
+ let passwordTwo = passwordItemsTwo.join('')
+   
+pswOne.textContent = passwordOne
+pswTwo.textContent = passwordTwo
+
+passwordItemsOne = []
+passwordItemsTwo = []
+
+ }
+
+
+ 
